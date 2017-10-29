dba61596526e6798dabc8fe16a6d1318da9cc98c
eikood <hanfaplast@googlemail.com>
2017-10-29 09:26:06 -0700

    finished

--- content.php
+++ content.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['login'])) {
   <a href="logout.php">Get out</a><br />
   <?php
   echo "You are logged in with session id: <br />".session_id()."<br />";
-  // echo "You secret token is: <br />" .$_SESSION['csrf_token']. "<br />";
+  echo "You secret token is: <br />" .$_SESSION['csrf_token']. "<br />";
   ?>
   <form action="content.php" method="GET">
     <input type="hidden" name="csrf" value="<?php echo $_SESSION['csrf_token']; ?>">
@@ -18,12 +18,12 @@ if (isset($_SESSION['login'])) {
   if(isset($_GET['write'])) {
     $myFile='text';
     $fh = fopen($myFile, 'a') or die("can’t open file");
-    //     if ($_GET['csrf'] == $_SESSION['csrf_token']) {
-    fwrite($fh, $_GET['write']."\n");
-    echo $_GET['write']." has been written down on file";
-    //      } else {
-    //     die("Wrong Token!!");
-    //   }
+    if ($_GET['csrf'] == $_SESSION['csrf_token']) {
+      fwrite($fh, $_GET['write']."\n");
+      echo $_GET['write']." has been written down on file";
+    } else {
+      die("Wrong Token!!");
+    }
   }
 }
 else {
